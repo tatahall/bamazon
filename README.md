aa4c250e5a25c72b2300af63bfa2aa00c3ef344c
Tahnee Hall <tnhall2012@hotmai.com>
2019-10-27 19:58:50 -0400

    correct sql file and updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # bamazon
 MySQL and Nodejs app
+
+* Purpose:
+The purpose of this application is to create a wholesale buying application using MySQL (database) and Node.js.
+
+* Organization:
+
+* There are two primary js documents:
+There are two code for this project is documented on two primary documents:
+1. bamazon.sql to hold the basic database.
+2. bamazonCustomer.js to hold the code necessary to allow users to make purchases.
+    
+
+App Instructions:
+* 1.
+
+Screenprints:
+<p><img src="/images/dbscrn1.png"/></p>
+<p><img src="/images/dbscrn2.png"/></p>
+
+
+Github repo link:
+
+
+
+Technology Used:
+* Node.js
+* MySQL Workbench
+
+
+Role:
+* I completed all the coding for this project.
---
+++ bamazon.sql
@@ -0,0 +1,19 @@
+-- create the database
+CREATE DATABASE bamazon_db;
+
+-- start the database
+USE bamazon_db;
+
+-- create table
+CREATE TABLE products (
+    id INT NOT NULL AUTO_INCREMENT,
+    item VARCHAR (45) NULL,
+    department VARCHAR (45) NULL,
+    price DECIMAL (10,2) NULL,
+    quantity INT NULL,
+    PRIMARY KEY (id)
+);
+
+-- insert data into the table
+INSERT INTO products (item, department, price, quantity)
+Values ("paper", "office supplies", 10.50, 200),("light bulbs", "home", 3.00, 100),("yarn - scarlet", "craft supplies", 4.50, 50),("chocolate bar", "food", 100, 100),("bread", "food", 1.95, 500),("comb", "health & beauty", 0.10, 1000),("towels - blue", "home", 15.99, 25),("curtains - beige", "home", 150, 35),("binder", "office supplies", 12.00, 75),("steak", "food", 16.55, 175);
